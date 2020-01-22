95ec74e968a6667ce47eb7fed73cc74bb96ea61b
r.gilham-ssg <rgilham@sensysgatso.com>
2020-01-22 19:37:56 +0100

    started adding tests

--- commands_test.go
+++ commands_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/eurozulu/mainline"
 	"net/url"
-	"os"
 	"testing"
 )
 
@@ -24,14 +23,38 @@ func (c MyCommands) DoThis(name string) {
 func (c MyCommands) DoThat(name string, lockernumber int) {
 	fmt.Printf("Do that name: %s locaker: %d isVerbose: %v, isDebug: %v\n", name, lockernumber, c.Verbose, c.Debug)
 }
+func (c MyCommands) DoTheNumbers(i int, f float32, b bool) {
+	fmt.Printf("the numbers are: int: %v float: %v bool: %v\n", i, f, b)
+}
 
 func TestCommands_AddCommand(t *testing.T) {
 
 	mainline.MustAddCommand("dothis", MyCommands.DoThis)
 	mainline.MustAddCommand("dothat", MyCommands.DoThat)
 
-	if err := mainline.RunCommandLine(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	if err := mainline.RunCommand("dothis", "world"); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := mainline.RunCommand("dothat", "world", "22"); err != nil {
+		t.Fatal(err)
+	}
+	if err := mainline.RunCommand("dothis"); err == nil {
+		t.Fatal(fmt.Errorf("expected error of not enough params"))
+	}
+	if err := mainline.RunCommand("dothis", "hello", "world"); err == nil {
+		t.Fatal(fmt.Errorf("expected error of too many params"))
+	}
+
+	if err := mainline.RunCommand("dothat", "hello", "world"); err == nil {
+		t.Fatal(fmt.Errorf("expected unparsable int error"))
+	}
+}
+
+func TestCommands_NumberParams(t *testing.T) {
+	mainline.MustAddCommand("numbers", MyCommands.DoTheNumbers)
+
+	if err := mainline.RunCommand("numbers", "1", "1.2", "true"); err != nil {
+		t.Fatal(err)
 	}
 }
