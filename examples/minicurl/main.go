1b24216793cdf0af8fc723a1c6d597c58cd44744
r.gilham-ssg <rgilham@sensysgatso.com>
2020-01-25 18:51:03 +0100

    tidied up minicurl and gave it help. Shows off very well how 'free' help text is available using helpmaker :-)

--- examples/minicurl/main.go
+++ examples/minicurl/main.go
@@ -1,5 +1,5 @@
 // minicurl is a silly example of how small you can make a functional tool.
-// Although crude, using two lines of code, we can get a http response header with one command.
+// Although crude, using a few lines of code, we can get a http response header with one command.
 // get <url>
 //
 // This example shows how base functions can be used, on packages other than your own application.
@@ -7,10 +7,12 @@ package main
 
 import (
 	"github.com/eurozulu/mainline"
+	"github.com/eurozulu/mainline/examples/minicurl/_help"
 	"net/http"
 )
 
 func main() {
 	mainline.MustAddCommand("get", http.Get)
+	mainline.MustAddCommand("help", _help.HelpCommand)
 	mainline.RunCommandLine()
 }
