444acd0b25a4738191ad46b533a96484859ef2ec
r.gilham-ssg <rgilham@sensysgatso.com>
2020-01-25 19:50:34 +0100

    Added apache copyright

--- examples/smallcurl/main.go
+++ examples/smallcurl/main.go
@@ -1,3 +1,18 @@
+// Copyright 2020 Rob Gilham
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 // smallcurl is another triviol example which builds on the mini curl.
 // Still keeping it simple, it adds a struct to handle the response.
 // With the struct it can use flags, so adds a flag to get the headers as well as the body
